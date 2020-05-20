7243d35d0c778656093064fc6eab5348b1c7fc0b
Andrzej Rafalski <anraf1001@gmail.com>
2020-05-20 19:56:45 +0200

    Textures are stored in a map now

--- configuration.hpp
+++ configuration.hpp
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <stdexcept>
+#include <map>
 
 void textConfig(sf::Text &text, const sf::Font &font, const std::string &textToWrite, const unsigned int &characterSize,
                 const sf::Color &color) {
@@ -12,9 +13,37 @@ void textConfig(sf::Text &text, const sf::Font &font, const std::string &textToW
     text.setFillColor(color);
 }
 
-void textureLoading(sf::Texture &textureToSet, const std::string &pathToTexture){
-    if(!textureToSet.loadFromFile(pathToTexture)){
-        throw std::invalid_argument("Couldn't load a texture");
+void loadTextures(std::map<std::string, sf::Texture> &textureMap) {
+    textureMap.insert({"pacman", sf::Texture()});
+    if (!textureMap.at("pacman").loadFromFile("../Assets/Images/pacman.png")) {
+        throw std::invalid_argument("Couldn't load a pacman's texture");
+    }
+
+    textureMap.insert({"wall", sf::Texture()});
+    textureMap.at("wall").setRepeated(true);
+    if (!textureMap.at("wall").loadFromFile("../Assets/Images/grayTile.png")) {
+        throw std::invalid_argument("Couldn't load a wall texture");
+    }
+
+    textureMap.insert({"start", sf::Texture()});
+    if (!textureMap.at("start").loadFromFile("../Assets/Images/greenTile.png")) {
+        throw std::invalid_argument("Couldn't load a starting tile texture");
+    }
+
+    textureMap.insert({"end", sf::Texture()});
+    if (!textureMap.at("end").loadFromFile("../Assets/Images/redTile.png")) {
+        throw std::invalid_argument("Couldn't load a ending tile texture");
+    }
+
+    textureMap.insert({"point", sf::Texture()});
+    if (!textureMap.at("point").loadFromFile("../Assets/Images/yellowTile.png")) {
+        throw std::invalid_argument("Couldn't load a point texture");
+    }
+
+    textureMap.insert({"background", sf::Texture()});
+    textureMap.at("background").setRepeated(true);
+    if (!textureMap.at("background").loadFromFile("../Assets/Images/darkGrayTile.png")) {
+        throw std::invalid_argument("Couldn't load a background texture");
     }
 }
 
