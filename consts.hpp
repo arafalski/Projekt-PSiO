ae670b3b8930907b3ccea761c97ea360499f8915
Andrzej Rafalski <anraf1001@gmail.com>
2020-05-31 12:08:51 +0200

    Update consts

--- consts.hpp
+++ consts.hpp
@@ -1,10 +1,11 @@
 #ifndef PROJEKT_CONSTS_HPP
 #define PROJEKT_CONSTS_HPP
 
-constexpr unsigned int WINDOW_HEIGHT = 600;
-constexpr unsigned int WINDOW_WIDTH = 800;
-constexpr float TILE = 80.0f;
-constexpr unsigned int MAP_WIDTH = 10;
-constexpr unsigned int MAP_HEIGHT = MAP_WIDTH;
+constexpr auto WINDOW_HEIGHT = 600;
+constexpr auto WINDOW_WIDTH = 800;
+constexpr auto TILE = 80.0f;
+constexpr auto MAP_WIDTH = 10;
+constexpr auto MAP_HEIGHT = MAP_WIDTH;
+constexpr auto LIGHT_WIDTH = 0.3927f; // PI / 8
 
 #endif //PROJEKT_CONSTS_HPP
